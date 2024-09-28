5a2baf59d007513763abed34bf35bc8d07077f1c
seraphh <brockpeuter@gmail.com>
2024-09-28 16:14:23 -0700

    ass

---
+++ demo1.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="output.php" method="POST">
+<div>
+<label for="username"> Username</label>
+<input type="text" name="username" id="username">
+</div>
+
+<div>
+<label for="email"> email</label>
+<input type="email" name="email" id="email">
+</div>
+
+<div>
+<label for="password"> password</label>
+<input type="password" name="password" id="password">
+</div>
+
+<div>
+<label for="confirm-password"> password</label>
+<input type="password" name="confirm-password" id="confirm-password">
+</div>
+
+<div>
+<input type="submit" value="Submit">
+</div>
+
+
+    </form>
+
+</body>
+</html>
---
+++ output.php
@@ -0,0 +1,48 @@
+<?php  
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $email = $_POST["email"];
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $confirm_password = $_POST ["confirm-password"];
+/*
+    if(trim($username) != ""){
+    echo "USER Email is Not Empty<br>";
+    
+}else {
+    echo "username is empty<br>";
+}
+
+if(trim($email) != ""){
+    echo "Email is Not Empty<br>";
+    
+}else {
+    echo "email is empty<br>";
+}
+
+if(trim($password) != ""){
+    echo "password is Not Empty<br>";
+    
+}else {
+    echo "password is empty<br>";
+}
+*/
+test($username);
+test(fieldname: $email);
+test($password);
+test($confirm_password);
+
+
+
+function test ($fieldname){
+    if(trim($fieldname) != ""){
+        echo "User Field is Not Empty<br>";
+        
+    }else {
+        echo "field is empty<br>";
+    }
+}
+
+}
+
+?>
